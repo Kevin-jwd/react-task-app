de77383bd4b5485453f5c12fa730dd625e0be75a
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-14 16:19:58 +0900

    feat(components/BoardList/BoardList.tsx): BoardList 컴포넌트 기본 구조 및 기능 구현

--- vite-project/src/components/BoardList/BoardList.tsx
+++ vite-project/src/components/BoardList/BoardList.tsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { FC, useState } from "react";
+import { useTypesSelector } from "../../hooks/redux";
+import SideForm from "./SideForm/SideForm";
+import { FiPlusCircle } from "react-icons/fi";
+import {
+    addSection,
+    container,
+    title,
+    addButton,
+    boardItemActive,
+    boardItem,
+} from "./BoardList.css";
+import clsx from "clsx";
 
-const BoardList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+type TBoardListProps = {
+    activeBoardId: string;
+    setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
+};
 
-export default BoardList
+const BoardList: FC<TBoardListProps> = ({
+    activeBoardId,
+    setActiveBoardId,
+}) => {
+    const { boardArray } = useTypesSelector((state) => state.boards);
+    const [isFormOpen, setIsFormOpen] = useState(false);
+    return (
+        <div className={container}>
+            <div className={title}>게시판 :</div>
+            {boardArray.map((board, index) => (
+                <div
+                    key={board.boardId}
+                    onClick={() => setActiveBoardId(boardArray[index].boardId)}
+                    className={clsx(
+                        {
+                            [boardItemActive]:
+                                boardArray.findIndex(
+                                    (b) => b.boardId === activeBoardId
+                                ) === index,
+                        },
+                        {
+                            [boardItem]:
+                                boardArray.findIndex(
+                                    (b) => b.boardId === activeBoardId
+                                ) !== index,
+                        }
+                    )}
+                >
+                    <div>{board.boardName}</div>
+                </div>
+            ))}
+            <div className={addSection}>
+                {isFormOpen ? (
+                    <SideForm setIsFormOpen={setIsFormOpen} />
+                ) : (
+                    <FiPlusCircle
+                        className={addButton}
+                        onClick={() => setIsFormOpen(!isFormOpen)}
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default BoardList;
