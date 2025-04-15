29b606c680a51ac20e603ef80bf2e3cb36c2e802
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-15 14:35:36 +0900

    feat: SideForm에 게시판 등록 기능 추가

--- vite-project/src/components/BoardList/SideForm/SideForm.tsx
+++ vite-project/src/components/BoardList/SideForm/SideForm.tsx
@@ -1,6 +1,11 @@
 import React, { ChangeEvent, FC } from "react";
 import { useState } from "react";
 import { FiCheck } from "react-icons/fi";
+import { icon, input, sideForm } from "./SideForm.css";
+import { useTypedDispatch } from "../../../hooks/redux";
+import { addBoard } from "../../../store/slices/boardsSlice";
+import { v4 as uuidv4 } from "uuid";
+import { addLog } from "../../../store/slices/loggerSlice";
 
 type TSideFormProps = {
     inputRef: React.RefObject<HTMLInputElement>;
@@ -9,6 +14,7 @@ type TSideFormProps = {
 
 const SideForm: FC<TSideFormProps> = ({ setIsFormOpen, inputRef }) => {
     const [inputText, setInputText] = useState("");
+    const dispatch = useTypedDispatch();
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setInputText(e.target.value);
@@ -16,18 +22,40 @@ const SideForm: FC<TSideFormProps> = ({ setIsFormOpen, inputRef }) => {
     const handleOnBlur = () => {
         setIsFormOpen(false);
     };
+    const handleClick = () => {
+        if (inputText) {
+            dispatch(
+                addBoard({
+                    board: {
+                        boardId: uuidv4(),
+                        boardName: inputText,
+                        lists: [],
+                    },
+                })
+            );
+            dispatch(
+                addLog({
+                    logId: uuidv4(),
+                    logMessage: `게시판 등록: ${inputText}`,
+                    logAuthor: "User",
+                    logTimestamp: String(Date.now()),
+                })
+            );
+        }
+    };
 
     return (
-        <div>
+        <div className={sideForm}>
             <input
                 autoFocus
+                className={input}
                 type="text"
                 placeholder="새로운 게시판 등록하기"
                 value={inputText}
                 onChange={handleChange}
                 onBlur={handleOnBlur}
             />
-            <FiCheck></FiCheck>
+            <FiCheck className={icon} onMouseDown={handleClick}></FiCheck>
         </div>
     );
 };
