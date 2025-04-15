45e6bc8549d4f7da2a99be133b3f9e706a3fe3fa
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-15 18:43:28 +0900

    feat: App 컴포넌트에 ListsContainer 컴포넌트 추가 및 상태 연동

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import { appContainer, board, buttons } from "./App.css";
 import BoardList from "./components/BoardList/BoardList";
+import ListsContainer from "./components/ListsContainer/ListsContainer";
+import { useTypesSelector } from "./hooks/redux";
 
 function App() {
     const [activeBoardId, setActiveBoardId] = useState("board-0");
+    const boards = useTypesSelector((state) => state.boards.boardArray);
+    const getActiveBoard = boards.filter(
+        (board) => board.boardId === activeBoardId
+    )[0];
+    const lists = getActiveBoard.lists;
 
     return (
         <div className={appContainer}>
@@ -11,7 +18,12 @@ function App() {
                 activeBoardId={activeBoardId}
                 setActiveBoardId={setActiveBoardId}
             ></BoardList>
-            <div className={board}></div>
+            <div className={board}>
+                <ListsContainer
+                    lists={lists}
+                    boardId={getActiveBoard.boardId}
+                ></ListsContainer>
+            </div>
             <div className={buttons}>
                 <button>이 게시판 삭제하기</button>
                 <button></button>
