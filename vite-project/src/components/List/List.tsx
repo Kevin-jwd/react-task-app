c98dc4f8d5ff4708f2cb66d3e8b3638e04f3c0b8
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-16 15:39:02 +0900

    style: List 컴포넌트에 스타일 적용

--- vite-project/src/components/List/List.tsx
+++ vite-project/src/components/List/List.tsx
@@ -8,6 +8,7 @@ import { deleteList, setModalActive } from "../../store/slices/boardsSlice";
 import { addLog } from "../../store/slices/loggerSlice";
 import { v4 } from "uuid";
 import { setModalData } from "../../store/slices/modalSlice";
+import { deleteButton, header, listWrapper, name } from "./List.css";
 
 type TListProps = {
     boardId: string;
@@ -16,34 +17,47 @@ type TListProps = {
 
 const List: FC<TListProps> = ({ list, boardId }) => {
     const dispatch = useTypedDispatch();
-    const handleListDelete = (listId: string)=>{
-        dispatch(deleteList({boardId, listId}))
+    const handleListDelete = (listId: string) => {
+        dispatch(deleteList({ boardId, listId }));
         dispatch(
             addLog({
                 logId: v4(),
                 logMessage: `리스트 삭제하기: ${list.listName}`,
                 logAuthor: "User",
-                logTimestamp: String(Date.now())
+                logTimestamp: String(Date.now()),
             })
-        )
-    }
+        );
+    };
     const handleTaskChange = (
-        boardId: string, 
-        listId: string, 
+        boardId: string,
+        listId: string,
         taskId: string,
         task: ITask
-        ) => {
-            dispatch(setModalData({boardId, listId, task}))
-            dispatch(setModalActive(true))
-    }
+    ) => {
+        dispatch(setModalData({ boardId, listId, task }));
+        dispatch(setModalActive(true));
+    };
     return (
-        <div>
-            <div>
-                <div>{list.listName}</div>
-                <GrSubtract onClick={() => handleListDelete(list.listId)}></GrSubtract>
+        <div className={listWrapper}>
+            <div className={header}>
+                <div className={name}>{list.listName}</div>
+                <GrSubtract
+                    className={deleteButton}
+                    onClick={() => handleListDelete(list.listId)}
+                ></GrSubtract>
             </div>
             {list.tasks.map((task, index) => (
-                <div key={task.taskId} onClick={() => handleTaskChange(boardId, list.listId, task.taskId, task)}>
+                <div
+                    key={task.taskId}
+                    onClick={() =>
+                        handleTaskChange(
+                            boardId,
+                            list.listId,
+                            task.taskId,
+                            task
+                        )
+                    }
+                >
                     <Task
                         taskName={task.taskName}
                         taskDescription={task.taskDescription}
